a28c0d55853d5699fb3bcfe5cd3439bb97da5750
Aybüke Durmuş <aybukedrms2003@gmail.com>
2025-08-28 17:07:49 +0300

    1

--- Models/Slider/SliderCreateModel.cs
+++ Models/Slider/SliderCreateModel.cs
@@ -5,7 +5,7 @@ public class SliderCreateModel
     public string? Baslik { get; set; }
     public string? Aciklama { get; set; }
     public IFormFile? Resim { get; set; } = null!;
-    public int Index { get; set; }//1111
+    public int Index { get; set; }//111111
     public bool Aktif { get; set; }
 }
 
